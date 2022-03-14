412dd30afba5bea63ca07b55461b6d7ca91968a7
Hyunsik Kim <devconf5296@gmail.com>
2022-03-14 20:01:56 +0900

    Use Java Observer API

--- observer-pattern/src/main/java/CurrentConditionDisplay.java
+++ observer-pattern/src/main/java/CurrentConditionDisplay.java
@@ -1,26 +1,30 @@
 import DisplayElement.DisplayElement;
-import Observer.Observer;
-import Subject.Subject;
+
+import java.util.Observable;
+import java.util.Observer;
 
 public class CurrentConditionDisplay implements Observer, DisplayElement {
     private float temperature;
     private float humidity;
-    private Subject weatherData;
+    private Observable observable;
 
-    public CurrentConditionDisplay(Subject weatherData){
-        this.weatherData = weatherData;
-        weatherData.registerObserver(this);
-    }
-
-    @Override
-    public void update(float temp, float humidity, float pressure) {
-        this.temperature= temp;
-        this.humidity= humidity;
-        display();
+    public CurrentConditionDisplay(Observable observable){
+        this.observable =observable;
+        observable.addObserver(this);
     }
 
     @Override
     public void display() {
         System.out.println("Current conditions: "+ this.temperature + "F degrees and " + this.humidity + "% humidity");
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if(o instanceof WeatherData){
+            WeatherData weatherData = (WeatherData)o;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
     }
 }
--- observer-pattern/src/main/java/WeatherData.java
+++ observer-pattern/src/main/java/WeatherData.java
@@ -1,45 +1,17 @@
-import Observer.Observer;
-import Subject.Subject;
+import java.util.Observable;
 
-import java.util.ArrayList;
-
-public class WeatherData implements Subject {
-    private final ArrayList<Observer> observers;
-
+public class WeatherData extends Observable {
     private float temperature;
 
     private float humidity;
 
     private float pressure;
 
-    public WeatherData(){
-        this.observers = new ArrayList();
-    }
-
-    @Override
-    public void registerObserver(Observer o) {
-        this.observers.add(o);
-    }
-
-    @Override
-    public void removeObserver(Observer o) {
-        int i= observers.indexOf(o);
-
-        if(i>0){
-            this.observers.remove(i);
-        }
-    }
-
-    @Override
-    public void notifyObserve() {
-        for(int i=0;i<observers.size();i++){
-            Observer observer = this.observers.get(i);
-            observer.update(this.temperature, this.humidity, this.pressure);
-        }
-    }
+    public WeatherData(){ }
 
     public void measurementsChanged(){
-        notifyObserve();
+        setChanged();
+        notifyObservers();
     }
 
     public void setMeasurements(float temperature, float humidity, float pressure){
@@ -47,5 +19,17 @@ public class WeatherData implements Subject {
         this.humidity=humidity;
         this.pressure=pressure;
         measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
     }
 }
